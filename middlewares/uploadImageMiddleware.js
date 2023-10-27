4546b768395fd07b596786d954f164b4962aec1b
Abd_El Rahman Mohamed <apdomedo6@gmail.com>
2023-10-27 19:21:52 +0200

    update upload middleware

--- middlewares/uploadImageMiddleware.js
+++ middlewares/uploadImageMiddleware.js
@@ -7,13 +7,19 @@ const multerOptions = () => {
   const multerStorage = multer.memoryStorage();
 
   const multterFilter = function (req, file, cb) {
-    if (
-      file.mimetype.startsWith("image") ||
-      file.mimetype === "application/pdf"
-    ) {
+    const allowedMimeTypes = [
+      "image/jpeg",
+      "image/png",
+      "application/pdf",
+      "application/msword",  // Add this for Word files
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document" // Add this for Word files
+    ];
+   
+
+    if (allowedMimeTypes.includes(file.mimetype)) {
       cb(null, true);
     } else {
-      cb(new ApiError("Only image Allowed", 400), false);
+      cb(new ApiError("Only images, PDFs, and Word documents are allowed", 400), false);
     }
   };
   const upload = multer({ storage: multerStorage, fileFilter: multterFilter });
