705e5323813ef3e9942c4415ff4f698fe8732913
abdelrahman464 <apdomedo6@gmail.com>
2024-01-07 13:31:09 +0200

    chang user comment to allow null true

--- models/User.js
+++ models/User.js
@@ -33,7 +33,7 @@ module.exports = (db, DataTypes) => {
     },
     comment: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
     },
     emailVerifyCode: {
       type: DataTypes.STRING,
